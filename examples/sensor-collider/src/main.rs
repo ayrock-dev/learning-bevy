0357ae1a87d9681a973adb36ee4434d41a7e58ca
CarpOfTruth <tjlee2108@gmail.com>
2023-06-15 14:10:16 -0700

    Added door handling.

--- examples/sensor-collider/src/main.rs
+++ examples/sensor-collider/src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::tracing::field::display};
 use bevy_rapier2d::prelude::*;
 
 #[derive(Component)]
@@ -7,6 +7,12 @@ struct Person;
 struct Name(String);
 #[derive(Resource)]
 struct GreetTimer(Timer);
+#[derive(Component)]
+struct Player;
+#[derive(Component)]
+struct DoorUser;
+#[derive(Component)]
+struct Door(String);
 
 pub struct HelloPlugin;
 
@@ -20,18 +26,20 @@ impl Plugin for HelloPlugin {
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_plugin(HelloPlugin)
-    .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-    .add_plugin(RapierDebugRenderPlugin::default())
-    .add_startup_system(setup_graphics)
-    .add_startup_system(setup_physics)
-    .add_system(print_ball_altitude)
-    .run();
+        .add_plugins(DefaultPlugins)
+        // .add_plugin(HelloPlugin)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugin(RapierDebugRenderPlugin::default())
+        .add_startup_system(setup_graphics)
+        .add_startup_system(setup_physics)
+        .add_system(loop_ball_altitude)
+        .add_system(display_door_use)
+        .add_system(mark_door_users)
+        .run();
 }
 
 fn hello_world() {
-    println!("hello world!");
+    println!("Hello world!");
 }
 
 fn add_people(mut commands: Commands) {
@@ -57,18 +65,66 @@ fn setup_physics(mut commands: Commands) {
     /* Create the ground. */
     commands
         .spawn(Collider::cuboid(500.0, 50.0))
-        .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)));
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)))
+        .insert(Sensor)
+        .insert(Door(String::from("Level 1")));
 
     /* Create the bouncing ball. */
     commands
         .spawn(RigidBody::Dynamic)
         .insert(Collider::ball(50.0))
         .insert(Restitution::coefficient(0.7))
-        .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)))
+        .insert(Player)
+        .insert(DoorUser);
 }
 
-fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
-    for transform in positions.iter() {
-        println!("Ball altitude: {}", transform.translation.y);
+fn loop_ball_altitude(mut positions: Query<&mut Transform, With<RigidBody>>) {
+    for mut transform in positions.iter_mut() {
+        if transform.translation.y < -200.0 {
+            transform.translation.y = 400.0;
+        }
+    }
+}
+
+fn display_door_use(
+    mut commands: Commands,
+    rapier_context: Res<RapierContext>,
+    doors: Query<(Entity, &Door)>,
+    players: Query<Entity, (With<Player>, With<DoorUser>)>,
+) {
+    let (entity, door) = doors.single(); // A first entity with a collider attached.
+    if let Ok(player) = players.get_single() {
+        // A second entity with a collider attached.
+
+        /* Find the intersection pair, if it exists, between two colliders. */
+        if rapier_context.intersection_pair(entity, player) == Some(true) {
+            println!(
+                "The entities {:?} and {:?} have intersecting colliders!",
+                door.0, player
+            );
+            commands.entity(player).remove::<DoorUser>();
+        }
+    }
+}
+
+fn mark_door_users(
+    mut commands: Commands,
+    rapier_context: Res<RapierContext>,
+    doors: Query<(Entity, &Door)>,
+    players: Query<Entity, (With<Player>, Without<DoorUser>)>,
+) {
+    let (entity, door) = doors.single(); // A first entity with a collider attached.
+    if let Ok(player) = players.get_single() {
+        // A second entity with a collider attached.
+
+        /* Find the intersection pair, if it exists, between two colliders. */
+        if rapier_context.intersection_pair(entity, player) != Some(true) {
+            println!(
+                "The entities {:?} and {:?} no longer have intersecting colliders!",
+                door.0, player
+            );
+            commands.entity(player).insert(DoorUser);
+        }
     }
 }
