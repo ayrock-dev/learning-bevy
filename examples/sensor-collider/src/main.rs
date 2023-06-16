2f5473117200ef0ec64c61b806e0d93e463fae64
Eric Lee <ayrocklee@gmail.com>
2023-06-15 18:17:40 -0700

    make player worldly in example

--- examples/sensor-collider/src/main.rs
+++ examples/sensor-collider/src/main.rs
@@ -20,6 +20,8 @@ struct PlayerBundle {
   #[sprite_bundle("player.png")]
   #[bundle]
   pub sprite_bundle: SpriteBundle,
+  #[worldly]
+  pub worldly: Worldly,
 
   pub player: Player,
 }
@@ -55,6 +57,9 @@ fn main() {
         //.add_system(loop_ball_altitude)
         //.add_system(display_door_use)
         //.add_system(mark_door_users)
+        .add_system(movement)
+        .add_system(follow_camera)
+        .add_system(update_level_selection)
         .run();
 }
 
@@ -85,6 +90,63 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         ldtk_handle,
         ..Default::default()
     });
+}
+
+fn follow_camera(mut cameras: Query<&mut Transform, With<Camera>>, players: Query<&Transform, (With<Player>, Without<Camera>)>) {
+  for player_transform in players.iter() {
+    for mut camera_transform in cameras.iter_mut() {
+      camera_transform.translation.x = player_transform.translation.x;
+      camera_transform.translation.y = player_transform.translation.y;
+    }
+  }
+}
+
+fn movement(
+  input: Res<Input<KeyCode>>,
+  mut query: Query<(&mut Transform), With<Player>>,
+) {
+  for (mut transform) in &mut query {
+      let right = if input.pressed(KeyCode::D) { 1. } else { 0. };
+      let left = if input.pressed(KeyCode::A) { 1. } else { 0. };
+
+      transform.translation.x += (right - left) * 8.;
+
+      let up = if input.pressed(KeyCode::W) { 1. } else { 0. };
+      let down = if input.pressed(KeyCode::S) { 1. } else { 0. };
+
+      transform.translation.y += (up - down) * 8.;
+  }
+}
+
+fn update_level_selection(
+  level_query: Query<(&Handle<LdtkLevel>, &Transform), Without<Player>>,
+  player_query: Query<&Transform, With<Player>>,
+  mut level_selection: ResMut<LevelSelection>,
+  ldtk_levels: Res<Assets<LdtkLevel>>,
+) {
+  for (level_handle, level_transform) in &level_query {
+    
+      if let Some(ldtk_level) = ldtk_levels.get(level_handle) {
+          let level_bounds = Rect {
+              min: Vec2::new(level_transform.translation.x, level_transform.translation.y),
+              max: Vec2::new(
+                  level_transform.translation.x + ldtk_level.level.px_wid as f32,
+                  level_transform.translation.y + ldtk_level.level.px_hei as f32,
+              ),
+          };
+
+          for player_transform in &player_query {
+              if player_transform.translation.x < level_bounds.max.x
+                  && player_transform.translation.x > level_bounds.min.x
+                  && player_transform.translation.y < level_bounds.max.y
+                  && player_transform.translation.y > level_bounds.min.y
+                  && !level_selection.is_match(&0, &ldtk_level.level)
+              {
+                  *level_selection = LevelSelection::Iid(ldtk_level.level.iid.clone());
+              }
+          }
+      }
+  }
 }
 
 fn setup_physics(mut commands: Commands) {
